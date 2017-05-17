750b09dc20b4798db1fa431ae246e117afc966bc
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-05-17 14:16:22 -0300

    Logger for main class and format.

--- stylegenerator/stylegenerator-reader-cli/src/main/java/org/stylegenerator/reader/cli/Main.java
+++ stylegenerator/stylegenerator-reader-cli/src/main/java/org/stylegenerator/reader/cli/Main.java
@@ -10,20 +10,19 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
 import stylegenerator.core.Sentence;
 import stylegenerator.reader.TextsToStyle;
 
+@Slf4j
 public class Main {
 
 	private static final String COHERENCE_PARAMETER = "c";
 	private static final String DIR_PARAMETER = "d";
 	private static final String FILE_PARAMETER = "f";
-	static Logger logger = LoggerFactory.getLogger(Main.class);
 
 	private static void showOptions(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
@@ -41,7 +40,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		logger.info("Starting ...");
+		log.info("Starting ...");
 		CommandLineParser parser = new DefaultParser();
 		Options options = getOptions();
 
@@ -49,40 +48,39 @@ public class Main {
 			CommandLine line = parser.parse(options, args);
 
 			if (!line.hasOption(FILE_PARAMETER) && !line.hasOption(DIR_PARAMETER)) {
-				logger.warn("Please provide at least one file or directory");
+				log.warn("Please provide at least one file or directory");
 				showOptions(options);
 				return;
 			}
 
-			List<String> filesNames = line.hasOption(FILE_PARAMETER) ? Arrays.asList(line.getOptionValues(FILE_PARAMETER))
-					: Collections.<String> emptyList();
-			List<String> directoriesNames = line.hasOption(DIR_PARAMETER) ? Arrays.asList(line.getOptionValues(DIR_PARAMETER))
-					: Collections.<String> emptyList();
+			List<String> filesNames = line.hasOption(FILE_PARAMETER)
+					? Arrays.asList(line.getOptionValues(FILE_PARAMETER)) : Collections.<String> emptyList();
+			List<String> directoriesNames = line.hasOption(DIR_PARAMETER)
+					? Arrays.asList(line.getOptionValues(DIR_PARAMETER)) : Collections.<String> emptyList();
 
-			logger.debug(filesNames.toString());
-			logger.debug(directoriesNames.toString());
-			
-			
+			log.debug(filesNames.toString());
+			log.debug(directoriesNames.toString());
+
 			Integer coherence = Integer.valueOf(line.getOptionValue(COHERENCE_PARAMETER, "3"));
 
 			TextsToStyle analyzer = new TextsToStyle();
 
 			List<Sentence> sentences = analyzer.process(filesNames, directoriesNames, coherence);
 
-			sentences.forEach(tf -> logger.debug(tf.toString()));
-			
+			sentences.forEach(tf -> log.debug(tf.toString()));
+
 			ObjectMapper mapper = new ObjectMapper();
-			
+
 			String json = mapper.writeValueAsString(sentences);
-			
-			logger.debug(json);
-			
-			logger.info("Finished");
+
+			log.debug(json);
+
+			log.info("Finished");
 		} catch (ParseException e) {
-			logger.error("Invalid Command Line: " + e.getMessage(), e);
+			log.error("Invalid Command Line: " + e.getMessage(), e);
 			showOptions(options);
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			log.error(e.getMessage(), e);
 		}
 
 	}
