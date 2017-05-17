ad8017e5896d76a4e038e7cf82e6b08555319de9
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-05-17 15:06:17 -0300

    Output file.

---
+++ stylegenerator/stylegenerator-core/src/main/java/stylegenerator/core/Constants.java
@@ -0,0 +1,9 @@
+package stylegenerator.core;
+
+public class Constants {
+
+	public static final String DEFAULT_FILE_NAME_PATTERN = "yyyy-MM-dd-HH-MM-ss";
+	
+	public static final String FILE_STYLE_EXTENSION = ".style.json";
+
+}
--- stylegenerator/stylegenerator-reader-cli/src/main/java/org/stylegenerator/reader/cli/Main.java
+++ stylegenerator/stylegenerator-reader-cli/src/main/java/org/stylegenerator/reader/cli/Main.java
@@ -1,5 +1,10 @@
 package org.stylegenerator.reader.cli;
 
+import java.io.File;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,6 +19,7 @@ import org.apache.commons.cli.ParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+import stylegenerator.core.Constants;
 import stylegenerator.core.Sentence;
 import stylegenerator.reader.TextsToStyle;
 
@@ -23,6 +29,8 @@ public class Main {
 	private static final String COHERENCE_PARAMETER = "c";
 	private static final String DIR_PARAMETER = "d";
 	private static final String FILE_PARAMETER = "f";
+	private static final String OUTPUT_DIR_PARAMETER = "od";
+	private static final String OUTPUT_FILE_PARAMETER = "of";
 
 	private static void showOptions(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
@@ -35,8 +43,26 @@ public class Main {
 		options.addOption(FILE_PARAMETER, true, "File to be read (*.txt extension)");
 		options.addOption(DIR_PARAMETER, true, "Directory to read. All *.txt files will be read");
 		options.addOption(COHERENCE_PARAMETER, true, "Coherence level (Default = 3)");
+		options.addOption(OUTPUT_DIR_PARAMETER, true, "Output directory (default current directory)");
+		options.addOption(OUTPUT_FILE_PARAMETER, true,
+				"Output file name, with extension '*.style.json' (default pattern yyyy-MM-dd-HH-MM-ss)");
 
 		return options;
+	}
+
+	private static File prepareOutputFile(String dirPath, String fileName) {
+		File outputDir = (dirPath == null) ? new File(".") : new File(dirPath);
+		if (!outputDir.exists() || !outputDir.isDirectory()) {
+			throw new IllegalArgumentException(
+					"Output directory " + dirPath + " does not correspond to a real directory");
+		}
+
+		String outputFileName = (fileName == null)
+				? LocalDateTime.now().format(DateTimeFormatter.ofPattern(Constants.DEFAULT_FILE_NAME_PATTERN))
+				: fileName;
+
+		return new File(outputDir, outputFileName + Constants.FILE_STYLE_EXTENSION);
+
 	}
 
 	public static void main(String[] args) {
@@ -52,6 +78,8 @@ public class Main {
 				showOptions(options);
 				return;
 			}
+			File outputFile = prepareOutputFile(line.getOptionValue(OUTPUT_DIR_PARAMETER),
+					line.getOptionValue(OUTPUT_FILE_PARAMETER));
 
 			List<String> filesNames = line.hasOption(FILE_PARAMETER)
 					? Arrays.asList(line.getOptionValues(FILE_PARAMETER)) : Collections.<String> emptyList();
@@ -71,9 +99,9 @@ public class Main {
 
 			ObjectMapper mapper = new ObjectMapper();
 
-			String json = mapper.writeValueAsString(sentences);
+			mapper.writerWithDefaultPrettyPrinter().writeValue(outputFile, sentences);
 
-			log.debug(json);
+			log.debug("Style file {} Created", outputFile.getAbsolutePath());
 
 			log.info("Finished");
 		} catch (ParseException e) {
