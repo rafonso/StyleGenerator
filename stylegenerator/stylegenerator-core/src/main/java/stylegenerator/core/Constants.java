61ea29e29dfa7885af10702409d0cde9334c94ef
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-05-20 16:53:56 -0300

    Output file creation

--- stylegenerator/stylegenerator-core/src/main/java/stylegenerator/core/Constants.java
+++ stylegenerator/stylegenerator-core/src/main/java/stylegenerator/core/Constants.java
@@ -6,13 +6,15 @@ import java.util.Random;
 
 public class Constants {
 
-	public static final String DEFAULT_FILE_NAME_PATTERN = "yyyy-MM-dd-HH-MM-ss";
+	public static final String DEFAULT_FILE_NAME_PATTERN = "yyyy-MM-dd-HH-mm-ss";
 	
 	public static final String FILE_STYLE_EXTENSION = ".style.json";
+	
+	public static final String GENERATED_FILE_EXTENSION = ".generated.txt";
 
 	public static final String EOL = System.getProperty("line.separator");
 
-	public static final Charset CHARSET = StandardCharsets.UTF_8;
+	public static final Charset CHARSET = StandardCharsets.ISO_8859_1;
 	
 	public static final Random RANDOM = new Random();
 
--- stylegenerator/stylegenerator-writer-cli/src/main/java/stylegenerator/writer/cli/Main.java
+++ stylegenerator/stylegenerator-writer-cli/src/main/java/stylegenerator/writer/cli/Main.java
@@ -2,7 +2,13 @@ package stylegenerator.writer.cli;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -25,6 +31,8 @@ import stylegenerator.textgeneration.TextParameter;
 @Slf4j
 public class Main {
 
+	private static final String STYLE_FILE_DESCRIPTION = "*.nn" + Constants.GENERATED_FILE_EXTENSION
+			+ " extension, where 'nn' is the coherence level";
 	private static final String FILE_PARAMETER = "f";
 	private static final String WAIT_FOR_END_OF_TEXT = "eot";
 	private static final String WORDS_QUANTITY_PARAMETER = "w";
@@ -35,6 +43,9 @@ public class Main {
 	private static final String OUTPUT_FILE_PARAMETER = "of";
 	private static final String HELPER_PARAMETER = "h";
 
+	private static final String STYLE_FILE_PATTERN = ".*(\\\\|/)([^.]+)\\.(\\d+)"
+			+ Constants.FILE_STYLE_EXTENSION.replaceAll("\\.", "\\\\.") + "$";
+
 	private static void showOptions(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
 		formatter.printHelp("Main", options);
@@ -43,7 +54,7 @@ public class Main {
 	private static Options getOptions() {
 		Options options = new Options();
 
-		options.addOption(FILE_PARAMETER, true, "Style File to be read (*.style.json extension)");
+		options.addOption(FILE_PARAMETER, true, "Style File to be read (" + STYLE_FILE_DESCRIPTION + ")");
 		options.addOption(WAIT_FOR_END_OF_TEXT, false, "Generates until the first End of Text");
 		options.addOption(WORDS_QUANTITY_PARAMETER, true, "Quantity of words to be generated");
 		options.addOption(WAIT_FOR_END_OF_PHRASE_PARAMETER, false,
@@ -53,7 +64,8 @@ public class Main {
 		options.addOption(OUTPUT_DIR_PARAMETER, true, "Output directory (default current directory)."
 				+ " The output file will be generated only this or " + OUTPUT_FILE_PARAMETER + " where passed.");
 		options.addOption(OUTPUT_FILE_PARAMETER, true,
-				"Output file name, with extension '*.generated.txt' (default pattern [STYLE FILE NAME].yyyy-MM-dd-HH-MM-ss)."
+				"Output file name, with extension " + Constants.GENERATED_FILE_EXTENSION
+						+ " (default pattern [STYLE FILE NAME].yyyy-MM-dd-HH-MM-ss)."
 						+ " The output file will be generated only this or " + OUTPUT_DIR_PARAMETER + " where passed.");
 		options.addOption(HELPER_PARAMETER, false, "Prints Usage");
 
@@ -62,18 +74,22 @@ public class Main {
 
 	private static void validateParameters(CommandLine line) {
 		if (!line.hasOption(FILE_PARAMETER)) {
-			throw new IllegalArgumentException("Please provide the style file (*.style.json extension)");
+			throw new IllegalArgumentException("Please provide the style file (" + STYLE_FILE_DESCRIPTION + ")");
+		}
+		if (!line.getOptionValue(FILE_PARAMETER).matches(STYLE_FILE_PATTERN)) {
+			throw new IllegalArgumentException("Style file shoud have " + STYLE_FILE_DESCRIPTION + ". But his name was "
+					+ line.getOptionValue(FILE_PARAMETER));
 		}
 		if (!line.hasOption(WAIT_FOR_END_OF_TEXT) //
 				&& !line.hasOption(LINES_QUANTITY_PARAMETER) //
 				&& !line.hasOption(PHRASES_QUANTITY_PARAMETER) //
 				&& !line.hasOption(WORDS_QUANTITY_PARAMETER)) {
-			throw new IllegalArgumentException(String.format(
-					"At least ont of these parameters must be passesd: '%s', '%s', '%s', '%s'", //
-					WAIT_FOR_END_OF_TEXT, //
-					LINES_QUANTITY_PARAMETER, //
-					PHRASES_QUANTITY_PARAMETER, //
-					WORDS_QUANTITY_PARAMETER));
+			throw new IllegalArgumentException(
+					String.format("Please provide at least ont of these parameters: '%s', '%s', '%s', '%s'", //
+							WAIT_FOR_END_OF_TEXT, //
+							LINES_QUANTITY_PARAMETER, //
+							PHRASES_QUANTITY_PARAMETER, //
+							WORDS_QUANTITY_PARAMETER));
 		}
 	}
 
@@ -127,14 +143,51 @@ public class Main {
 		return sentences;
 	}
 
+	private static Path getOutputFile(CommandLine line) {
+		String styleFileParameter = line.getOptionValue(FILE_PARAMETER);
+		Matcher styleFileMatcher = Pattern.compile(STYLE_FILE_PATTERN).matcher(styleFileParameter);
+		styleFileMatcher.matches();
+		String generatedFileNameTemination = "."
+				+ LocalDateTime.now().format(DateTimeFormatter.ofPattern(Constants.DEFAULT_FILE_NAME_PATTERN)) + "."
+				+ styleFileMatcher.group(3) + Constants.GENERATED_FILE_EXTENSION;
+
+		String generatedDirPath = line.getOptionValue(OUTPUT_DIR_PARAMETER, ".");
+		String generatedFileName = line.getOptionValue(OUTPUT_FILE_PARAMETER, styleFileMatcher.group(2))
+				+ generatedFileNameTemination;
+
+		File generatedDir = new File(generatedDirPath);
+		if (!generatedDir.exists() || !generatedDir.isDirectory()) {
+			throw new IllegalArgumentException("Output directory " + generatedDir.getName() + " is not valid");
+		}
+
+		return new File(generatedDir, generatedFileName).toPath();
+	}
+
+	private static boolean createOutputFile(CommandLine line, String text) throws IOException {
+		if (!line.hasOption(OUTPUT_DIR_PARAMETER) && !line.hasOption(OUTPUT_FILE_PARAMETER)) {
+			return false;
+		}
+
+		Path outputFile = getOutputFile(line);
+
+		log.debug("Creating output File");
+		Files.write(outputFile, text.getBytes(Constants.CHARSET));
+		log.debug("Output File {} created", outputFile.getFileName());
+
+		return true;
+	}
+
 	public static void main(String[] args) {
+		System.setProperty("file.encoding", Constants.CHARSET.name());
+		
 		long t0 = System.currentTimeMillis();
 
-		CommandLineParser parser = new DefaultParser();
 		Options options = getOptions();
 		String text = null;
+		boolean outputFileCreated = false;
 
 		try {
+			CommandLineParser parser = new DefaultParser();
 			CommandLine line = parser.parse(options, args);
 
 			if (line.hasOption(HELPER_PARAMETER)) {
@@ -147,8 +200,9 @@ public class Main {
 			TextParameter parameter = commandLineToStyleParameter(line);
 			log.debug(parameter.toString());
 
-			log.info("Reading Style file.");
-			List<Sentence> sentences = readStyleFile(line.getOptionValue(FILE_PARAMETER));
+			String fileParameter = line.getOptionValue(FILE_PARAMETER);
+			log.info("Reading Style file {}", fileParameter);
+			List<Sentence> sentences = readStyleFile(fileParameter);
 
 			TextGenerator textGenerator = new TextGenerator(parameter);
 
@@ -156,7 +210,8 @@ public class Main {
 			text = textGenerator.generateText(sentences);
 			log.info("Text generated");
 
-			// TODO: IF to generate output file
+			outputFileCreated = createOutputFile(line, text);
+
 			log.info("Finished. Time: {} ms", (System.currentTimeMillis() - t0));
 		} catch (ParseException e) {
 			log.error("Invalid Command Line: " + e.getMessage(), e);
@@ -168,8 +223,7 @@ public class Main {
 			log.error(e.getMessage(), e);
 		}
 
-
-		if (text != null) {
+		if (text != null && !outputFileCreated) {
 			System.out.println(text);
 		}
 	}
